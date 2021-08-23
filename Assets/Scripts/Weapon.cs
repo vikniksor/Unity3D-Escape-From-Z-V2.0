dc8f5f641a444ca0317f0ed94878006af9fa7446
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-08-23 20:51:45 +0300

    add damage handling

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera playerCamera;
     [Tooltip("Distance of shot.")]
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30f;
 
 
     void Update()
@@ -19,8 +20,15 @@ public class Weapon : MonoBehaviour
     private void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, 
-                        out hit, range);
-        Debug.Log("I hit this thing: " + hit.transform.name);
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, 
+                        out hit, range))
+        {
+            Debug.Log("I hit this thing: " + hit.transform.name);
+            //TODO: add some hit effect for visual players
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else { return; }
     }
 }
