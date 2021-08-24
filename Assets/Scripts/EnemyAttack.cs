0db09a4a603fd1f79deab46457f04d9c04376202
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-08-24 13:27:56 +0300

    add damage to player

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
 
-    [SerializeField] Transform target;
+    PlayerHealth target;
     [SerializeField] float damage = 40f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = FindObjectOfType<PlayerHealth>();
     }
 
     public void AttackHitEvent()
     {
         if (target == null) { return; }
-        Debug.Log("bang bang");
+        target.TakeDamage(damage);
+        Debug.Log("bang bang");       
     }
 }
