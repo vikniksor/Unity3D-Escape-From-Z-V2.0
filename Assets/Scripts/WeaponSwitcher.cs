eb82e157d8c30d29d48c887e4e0a2e3bd379afa8
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-08-25 13:20:25 +0300

    add player input to select weapon

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,38 @@ public class WeaponSwitcher : MonoBehaviour
         SetWeaponActive();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        int prevWeapon = currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScrollWheel();
+
+        if (prevWeapon != currentWeapon) { SetWeaponActive(); }
+    }
+
+    private void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { currentWeapon = 0; }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) { currentWeapon = 1; }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) { currentWeapon = 2; }
+    }
+
+    private void ProcessScrollWheel()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (currentWeapon >= transform.childCount - 1) { currentWeapon = 0; }
+            else { currentWeapon++; }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (currentWeapon <= 0) { currentWeapon = transform.childCount - 1; }
+            else { currentWeapon--; }
+        }
+    }
+
     private void SetWeaponActive()
     {
         int weaponIndex = 0;
@@ -26,9 +59,5 @@ public class WeaponSwitcher : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
