660b0ca4df5462051b93cdc07ecd413063eff547
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-09-01 18:27:25 +0300

    add to player a simple flashlight

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minAngle = 40f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle <= minAngle) return;
+        else { myLight.spotAngle -= angleDecay * Time.deltaTime; }        
+    }
+}
